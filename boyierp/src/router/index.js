59243c164aad6164772311655cbcc0bdce369a7b
AkeQAQ <244454526@qq.com>
2021-08-10 22:38:02 +0800

    新增login,index vue 删除无用文件

--- boyierp/src/router/index.js
+++ boyierp/src/router/index.js
@@ -1,22 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/index',
+    name: 'Index',
+    component: () => import('../views/Index')
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/Login',
+    name: 'Login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: Login
   }
 ]
 
